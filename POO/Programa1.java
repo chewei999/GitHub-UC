f84ee458c7e8db53c8540c9010bbaac24e1e219b
chewei999 <chewei999@gmail.com>
2022-09-02 18:30:42 -0500

    poo

---
+++ POO/Programa1.java
@@ -0,0 +1,8 @@
+public class Programa1
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Bienvenidos a Programacion Orientada a Objetos");
+        System.out.println("Ahora usaremos Java");
+    }
+}

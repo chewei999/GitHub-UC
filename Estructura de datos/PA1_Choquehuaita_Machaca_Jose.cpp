f5e4dc21b41cdd8b120b02c712d73a32c828b141
chewei999 <chewei999@gmail.com>
2022-09-03 00:55:11 -0500

    pa1_comments

--- Estructura de datos/PA1_Choquehuaita_Machaca_Jose.cpp
+++ Estructura de datos/PA1_Choquehuaita_Machaca_Jose.cpp
@@ -2,7 +2,10 @@
 
 using namespace std;
 
-// Declaracion de constantes
+  /***************************/
+ /** Constants declaration **/
+/***************************/
+// Messages for user
 string initMsg = "Ingrese un numero entero entre 1 y 20: ";
 string initError = "ERROR...El numero debe ser entero y entre 0 y 20, vuelva a intentar.\n\n";
 

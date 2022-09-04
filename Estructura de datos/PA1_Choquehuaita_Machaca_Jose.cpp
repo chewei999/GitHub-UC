a954a16f05adf518a2b0eb5c4ae441fd4dab4b91
chewei999 <chewei999@gmail.com>
2022-09-04 03:53:24 -0500

    final pa1 estructura de datos

--- Estructura de datos/PA1_Choquehuaita_Machaca_Jose.cpp
+++ Estructura de datos/PA1_Choquehuaita_Machaca_Jose.cpp
@@ -2,22 +2,70 @@
 
 using namespace std;
 
-  /***************************/
- /** Constants declaration **/
-/***************************/
+/** CONSTATNS DECLARATION **/
 // Messages for user
 string initMsg = "Ingrese un numero entero entre 1 y 20: ";
-string initError = "ERROR...El numero debe ser entero y entre 0 y 20, vuelva a intentar.\n\n";
+string initError = "ERROR...El numero debe ser entero y entre 1 y 20, vuelva a intentar.\n\n";
 
-// Variables declaration
-float results[3];       // Array to store results
-int num;                // Number entered by user
-bool validInput;        // Boolean to validate data input
+/** FUNCTIONS DECLARATION **/
+//Function to validate input is an integer between 0 and 20
+bool askValidateInput(int *val);
 
-/** Functions definitions **/
-//Function to validate input is a integer between 0 and 20
-bool validateInput(int &num) {
-    if(!(cin >> num) or cin.get() != '\n') {    // If input is not a integer
+//Function to return the input squared
+int f1_power(int num);
+
+//Function to return the number of units that input is from 20 or 10 depending on whether it is even or odd
+int f2_units(int num);
+
+//Function to return the sum of integers between 1 and the input
+int f3_sum(int num);
+
+//Funtion to return de average of previous 3 functions
+float f4_average(int num);
+
+/** MAIN FUNCTION **/
+int main() {
+    
+    // Variables declaration
+    float results[3];       // Array to store results
+    int *num = new int;     // Number entered by user
+    bool validInput;        // Boolean to validate data input
+    bool even;              // Boolean if number is even
+
+    do { 
+        system("cls");
+        cout << initMsg;
+        validInput = askValidateInput(num);
+    } while(!validInput);
+
+    (*num%2 == 0)? even = true : even = false;
+    results[0] = f1_power(*num);
+    results[1] = f2_units(*num);
+    results[2] = f3_sum(*num);
+    results[3] = f4_average(*num);
+
+    for(int i=0; i<=3; i++) {
+        printf("%.2f\n", results[i]);
+    }
+
+    cout << "\nEl numero ingresado es: " << *num;
+    cout << "\nEl numero ingresado al cuadrado: " << results[0];
+    cout << "\nEl numero ingresado esta a " << results[1] << " unidades de ";
+    even? cout << "20" : cout << "10";
+    cout << "\nLa suma desde 1 hasta el numero ingresado es: " << results[2];
+    printf("\nEl promedio de los 3 valores anteriores es: %.2f\n\n", results[3]);
+
+    system("pause");
+
+    delete num;
+    return 0;
+}
+
+
+/** FUNCTIONS DEFINITION **/
+bool askValidateInput(int *val) {
+
+    if(!(cin >> *val) or cin.get() != '\n') {   // If input value is not the right type: Integer
         cin.clear();                            // we clear the stream
         cin.ignore(256, '\n');                  // discard previous input
         cout << initError;                      // show error message to user
@@ -25,7 +73,7 @@ bool validateInput(int &num) {
         return false;                           // return FALSE because of the wrong input
     }
     else {                                      // If input is a integer
-        if (num >= 0 and num <= 20) {           // we evaluate if input is bewteen 0 and 20
+        if (*val >= 1 and *val <= 20) {         // we evaluate if input is bewteen 1 and 20
             return true;                        // return TRUE as the input is between correct range
         }
         else {                                  // If input is not between the correct range
@@ -36,23 +84,16 @@ bool validateInput(int &num) {
     }
 }
 
-//Function to return the input squared
 int f1_power(int num) {
     return num*num;
 }
 
-//Function to return the number of units that input is from 20 or 10 depending on whether it is even or odd
 int f2_units(int num) {
-
-    if (num%2 == 0) {
-        return abs(20 - num);
-    }
-    else {
-        return abs(10 - num);
-    }
+    int units;
+    (num%2 == 0)? units = abs(20 - num) : units = abs(10 - num);
+    return units;
 }
 
-//Function to return the sum of integers between 1 and the input
 int f3_sum(int num) {
     int acum = 0;
     for (int i=1 ; i<=num; i++) {
@@ -61,29 +102,6 @@ int f3_sum(int num) {
     return acum;
 }
 
-//Funtion to return de average of previous 3 functions
-float f4_average(float num) {
+float f4_average(int num) {
     return (f1_power(num) + f2_units(num) + f3_sum(num))/3.0;
-}
-
-/** MAIN FUNCTION **/
-int main() {
-
-    do { 
-        system("cls");
-        cout << initMsg;
-        validInput = validateInput(num);
-    } while(!validInput);
-
-    results[0] = f1_power(num);
-    results[1] = f2_units(num);
-    results[2] = f3_sum(num);
-    results[3] = f4_average(num);
-
-    for(int i=0; i<=3;i++) {
-        printf("%.2f\n", results[i]);
-    }
-
-    system("pause");
-    return 0;
 }

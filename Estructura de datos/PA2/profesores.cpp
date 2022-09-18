adfbc2209a7662af7c7f831f4a2c969c819ece97
chewei999 <chewei999@gmail.com>
2022-09-18 18:37:35 -0500

    profesores.cpp final

--- Estructura de datos/PA2/profesores.cpp
+++ Estructura de datos/PA2/profesores.cpp
@@ -1,3 +1,10 @@
+/********************************************************
+ *  Docente		: Ing. EDSON RAUL LAZO ALVAREZ			*
+ *  Alumno		: JOSE ALONSO CHOQUEHUAITA MACHACA		*
+ * 	Asignatura	: Estructura de Datos					*
+ ********************************************************/
+
+
 #include <iostream>
 #include <limits>
 #include <string>
@@ -22,10 +29,12 @@ void clearInputShowErrorMessage(string errorMessage);	//Proceso
 int main() {
 	// Variables para procesamiento de datos
 	float	edadPromedio = 0;															// Para calcular el promedio de las edades de todos los profesores
-	int		edadMinima = -1;															// Para calcular la edad del profesor(a) mas joven (inicializamos en un valor muy bajo para las comparaciones)
+	int		edadMinima = 999;															// Para calcular la edad del profesor(a) mas joven (inicializamos en un valor muy alto para las comparaciones)
 	char	nombreProfJoven[25];														// Nombre del profesor mas joven
-	int		edadMaxima = 999;															// Para calcular la edad del profesor(a) de más edad  (inicializamos en un valor muy alto para las comparaciones)
+	int		edadMaxima = -1;															// Para calcular la edad del profesor(a) de más edad  (inicializamos en un valor muy bajo para las comparaciones)
 	char	nombreProfMayor[25];														// Nombre del profesor de mas edad
+	int		cantProfesorasMayorProm = 0;												// Cantidad de profesoras mujeres con edad mayor al promedio
+	int		cantProfesoresMenorProm = 0;												// Cantidad de profesores hombres con edad menor al promedio
 
 	/** Solicitud al usuario de la cantidad de profesores **/
 	do {
@@ -44,7 +53,6 @@ int main() {
 	profesor *plana_docente = new profesor[cantProf];			// Declaramos el vector de punteros de tipo 'profesor' de longitud definida por el usuario
 
 	///*************** INGRESO DE DATOS ***************///
-
 		/** Recorremos el arreglo para solicitar los datos al usuario **/
 		cin.get();													// cin.get para evitar que se trunque el primer caracter del nombre
 		for(int i=0; i<cantProf; i++) {
@@ -91,26 +99,89 @@ int main() {
 
 		}
 		/** Recorremos el arreglo para solicitar los datos al usuario **/
-	
 	///*************** INGRESO DE DATOS ***************///
 
 
 
 	///*************** PROCESAMIENTO DE DATOS ***************///
+		/** Calculo del promedio de edades **/
+		for(int i=0; i<cantProf; i++) {																				// Recorremos el arreglo
+			edadPromedio += plana_docente[i].edad;																	// para obtener la suma de todas las edades
+		}
+		edadPromedio /= cantProf;																					// Dividimos la suma de todas las edades entre la cantidad de profesores
+		/** Calculo del promedio de edades **/
 
-	/** Calculo del promedio de edades **/
+		/** Calculo de profesor mas joven **/
+		for(int i=0; i<cantProf; i++) {																				// Recorremos el arreglo
+			if(plana_docente[i].edad < edadMinima) {																// Si la edad del docente es menor a la edad minmima (valor inicial es 999)
+				edadMinima = plana_docente[i].edad;																	// ahora la edad minima es la edad del docente
+				memcpy(nombreProfJoven, plana_docente[i].nombre, sizeof(nombreProfJoven));							// Y tambien guardamos el nombre del docente con la funcion memcpy copiamos el arreglo de chars, 
+																													// que es el nombre, ya que no se puede hacer una asignacion directa con el operador "="
+			}
+		}
+		/** Calculo de profesor mas joven **/
 
+		/** Calculo de profesor de mas edad **/
+		for(int i=0; i<cantProf; i++) {																				// Recorremos el arreglo
+			if(plana_docente[i].edad > edadMaxima) {																// Si la edad del docente es mayor a la edad maxima (valor inicial es -1)
+				edadMaxima = plana_docente[i].edad;																	// ahora la edad maxima es la edad del docente
+				memcpy(nombreProfMayor, plana_docente[i].nombre, sizeof(nombreProfMayor));							// Y tambien guardamos el nombre del docente con la funcion memcpy copiamos el arreglo de chars, 
+																													// que es el nombre, ya que no se puede hacer una asignacion directa con el operador "="
+			}
+		}
+		/** Calculo de profesor de mas edad **/
+
+		/** Calculo de profesoras mujeres con edad mayor al promedio **/
+		for(int i=0; i<cantProf; i++) {																						// Recorremos el arreglo
+			if( (plana_docente[i].sexo == 'f' or plana_docente[i].sexo == 'F') and plana_docente[i].edad > edadPromedio) {	// Si es profesora mujer y con edad mayor al promedio
+				cantProfesorasMayorProm++;																					// Incrementamos el contador correspondiente
+			}
+		}
+		/** Calculo de profesoras mujeres con edad mayor al promedio **/
+
+		/** Calculo de profesores hombres con edad menor al promedio **/
+		for(int i=0; i<cantProf; i++) {																						// Recorremos el arreglo
+			if( (plana_docente[i].sexo == 'm' or plana_docente[i].sexo == 'M') and plana_docente[i].edad < edadPromedio) {	// Si es profesor hombre y con edad menor al promedio
+				cantProfesoresMenorProm++;																					// Incrementamos el contador correspondiente
+			}
+		}
+		/** Calculo de profesoras mujeres con edad mayor al promedio **/
 	///*************** PROCESAMIENTO DE DATOS ***************///
 
 
-	for(int i=0; i<cantProf; i++) {
-		// ** Solicitud del nombre del profesor(a) **//
-		cout << plana_docente[i].nombre << endl;
-		cout << plana_docente[i].sexo << endl;			
-		// ** Solicitud del nombre del profesor(a) **//
-	}
 
-	delete [] plana_docente;
+	///*************** VISUALIZACION DE DATOS ***************///
+		system("cls");													// Limpiamos la pantalla previo a mostrar los resultados
+		
+		for(int i=0;i<80;i++) cout<<"=";								// Mostramos
+		cout << "\n\t\t\t\tDATOS INGRESADOS\n";							// un encabezado
+		for(int i=0;i<80;i++) cout<<"=";								// para la seccion de datos
+		cout <<"\nN - NOMBRE - SEXO - EDAD\n";							// ingresados
+
+		/** Datos Ingresados **/
+		for(int i=0; i<cantProf; i++) {																				// Recorremos el arreglo
+			cout << i+1 << " - " << plana_docente[i].nombre << " - ";												// Para cada elemento del arreglo mostramos cada uno de los miembros de la estructura
+			(plana_docente[i].sexo=='f' or plana_docente[i].sexo=='F')? cout << "Femenino" : cout<<"Masculino";		// para el sexo, mostramos la palabra completo dependiendo del caracter almacenado
+			cout << " - " << plana_docente[i].edad << "\n";
+		}
+		for(int i=0;i<80;i++) cout<<"=";
+		/** Datos Ingresados **/
+
+		/** Resultados **/
+		cout << "\n\n\n";
+		for(int i=0;i<80;i++) cout<<"=";																				// Mostramos							
+		cout << "\n\t\t\t\tRESULTADOS\n";																				// un encabezado
+		for(int i=0;i<80;i++) cout<<"=";																				// para la seccion de resultados
+		cout << "\na) Edad Promedio del grupo de profesores:\t\t\t" << edadPromedio;									// Mostramos la edad promedio de todos los profesores
+		cout << "\nb) El profesor mas joven del grupo es:\t\t\t\t"<< nombreProfJoven;									// Mostramos el nombre del profesor mas joven
+		cout << "\nc) El profesor de mas edad del grupo es:\t\t\t"<< nombreProfMayor;									// Mostramos el nombre del profesor de mas edad
+		cout << "\nd) El numero de profesoras con edad mayor al promedio es:\t"<< cantProfesorasMayorProm;				// Mostramos la cantidad de profesoras con edad mayor al promedio
+		cout << "\ne) El numero de profesores con edad menor al promedio es:\t"<< cantProfesoresMenorProm << endl;		// Mostramos la cantidad de profesores con edad menor al promedio
+		for(int i=0;i<80;i++) cout<<"=";
+		/** Resultados **/
+	///*************** VISUALIZACION DE DATOS ***************///
+					
+	delete [] plana_docente;																							// Liberamos es espacio de memoria que habiamos reservado inicialmente
 	return 0;
 }
 

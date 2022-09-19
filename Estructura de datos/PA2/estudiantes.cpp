c46fc289565e00e14991ab43349edb1cd1e21afc
chewei999 <chewei999@gmail.com>
2022-09-18 21:38:01 -0500

    estudiantes_final

--- Estructura de datos/PA2/estudiantes.cpp
+++ Estructura de datos/PA2/estudiantes.cpp
@@ -11,14 +11,19 @@
 
 using namespace std;
 
-struct estudiante {				// Definimos la estructura 'profesor' con 3 miembros
-  char	nombre[25];				// Miembro de tipo char para el nombre
+struct estudiante {				// Definimos la estructura 'estudiante' con 4 miembros para almacenar los datos de todos los estudiantes
+  char	nombre[50];				// Miembro de tipo char para el nombre
   int	dni;					// Miembro de tipo int para el dni
   int	semestres;				// Miembro de tipo int para el numero de semestres cursados
   float	promedio;				// Miembro de tipo float para el promedio
 };
 
-int		cantEst;					// Para guardar la cantidad de estudiantes ingresados por el usuario
+struct estudiante2 {			// Definimos la estructura 'estudiante2' con 2 miembros para almacenar los datos solo de nombre y dni de algunos estudiantes 
+  char	nombre[50];				// Miembro de tipo char para el nombre
+  int	dni;					// Miembro de tipo int para el dni
+};
+
+int		cantEst;				// Para guardar la cantidad de estudiantes ingresados por el usuario
 
 // Variables de control
 bool	cantEstValida = false;							// Para evaluar la validez de la cantidad de estudiantes ingresados
@@ -29,13 +34,12 @@ void clearInputShowErrorMessage(string errorMessage);	//Proceso
 
 int main() {
 	// Variables para procesamiento de datos
-	float	edadPromedio = 0;															// Para calcular el promedio de las edades de todos los profesores
-	int		edadMinima = 999;															// Para calcular la edad del profesor(a) mas joven (inicializamos en un valor muy alto para las comparaciones)
-	char	nombreProfJoven[25];														// Nombre del profesor mas joven
-	int		edadMaxima = -1;															// Para calcular la edad del profesor(a) de más edad  (inicializamos en un valor muy bajo para las comparaciones)
-	char	nombreProfMayor[25];														// Nombre del profesor de mas edad
-	int		cantProfesorasMayorProm = 0;												// Cantidad de profesoras mujeres con edad mayor al promedio
-	int		cantProfesoresMenorProm = 0;												// Cantidad de profesores hombres con edad menor al promedio
+	int		mejorProm = -1;																	// Para obtener los datos del estudiante con mejor promedio  (inicializamos en un valor muy bajo para las comparaciones)
+	int		cantEstAprobados = 0;															// Para contabilizar la cantidad de estudiantes con promedio mayor o igual a 10.5
+	int		cantEstMenosMediaCarrera = 0;													// Para contabilizar la cantidad de estudiantes que hayan cursado menos de 5 semestres
+	char	estMejorPromNombre[50];															// El nombre del estudiante que tiene mejor promedio
+	int		estMejorPromDNI;																// El DNI del estudiante que tiene mejor promedio
+	int 	pos;																			// Variable para recorrer los vectores con data de estudiantes aprobados y con menos de 5 semestres cursados
 
 	/** Solicitud al usuario de la cantidad de estudiantes **/
 	do {
@@ -60,14 +64,14 @@ int main() {
 
 			//** Nombre del estudiante **//
 			system("cls");											// Limpiamos la pantalla para mejor visualizacion
-			cout << "[" << i+1 << "] " << "Nombre de docente: ";	// Mostramos el mensaje al usuario indicando que ingrese el nombre del docente
+			cout << "[" << i+1 << "] " << "Nombre de estudiante: ";	// Mostramos el mensaje al usuario indicando que ingrese el nombre del docente
 			if(i>0) cin.get();										// cin.get para evitar que se trunque el primer caracter del nombre
-			cin.getline(salon[i].nombre,25);						// Guardamos el valor ingresado en la posicion correspondiente del vector,
+			cin.getline(salon[i].nombre, 50);						// Guardamos el valor ingresado en la posicion correspondiente del vector,
 																	// y asignamos al miembro correspondiente de la estructura
 			//** Nombre del estudiante **//
 
 			//** DNI del estudiante **//
-			dniValido = false;																					// En cada lectura inicializamos la validez del Sexo en FALSO
+			dniValido = false;																					// En cada lectura inicializamos la validez del DNI en FALSO
 			do {
 				system("cls");																					// Limpiamos la pantalla para mejor visualizacion
 				cout << "[" << i+1 << "] Ingrese el DNI de " << salon[i].nombre << ": ";						// Mostramos el mensaje al usuario pidiendo el DNI del estudiante
@@ -81,22 +85,37 @@ int main() {
 			} while(!dniValido);																				// Si la entrada no es valida, continuamos pidiendo el DNI del estudiante
 			//** DNI del estudiante **//
 
-			//** Edad del profesor(a) **//
-			semestresValidos = false;																					// En cada lectura inicializamos la validez de la edad en FALSO
+			//** Semestres cursados **//
+			semestresValidos = false;																					// En cada lectura inicializamos la validez de la cantidad de semestres en FALSO
 			do {
-				system("cls");																					// Limpiamos la pantalla para mejor visualizacion
-				cout << "[" << i+1 << "] Ingrese la edad de " << salon[i].nombre << ": ";				// Mostramos el mensaje al usuario pidiendo la edad del docente
-				cin >> salon[i].edad;																	// Guardamos el valor ingresado en la posicion correspondiente del vector,
-																												// y asignamos al miembro correspondiente de la estructura
-				//Si todo va bien en la entrada de datos y el dato es un entero mayor o igual a 18 (Ponemos la restriccion que para ser docente, la persona debe ser mayor de edad)
-				if( cin.good() and salon[i].edad >= 18 ) {
-					semestresValidos = true;																			// El booleano edadValida es verdadero, indicando que la entrada es valida y que se continue con la ejecucion del programa
-				} else {																						// SINO
-					clearInputShowErrorMessage("Error...El docente debe ser mayor de edad."); 					// Si hay algun error, limpiamos el buffer de entrada y mostramos el respectivo mensaje al usuario
+				system("cls");																							// Limpiamos la pantalla para mejor visualizacion
+				cout << "[" << i+1 << "] Ingrese la cantidad de semetres cursados por " << salon[i].nombre << ": ";		// Mostramos el mensaje al usuario pidiendo la cantidad de semestres cursados por el estudiante
+				cin >> salon[i].semestres;																				// Guardamos el valor ingresado en la posicion correspondiente del vector,
+																														// y asignamos al miembro correspondiente de la estructura
+				//Si todo va bien en la entrada de datos y el dato es un entero entre 1 y 10 (El estudiante no podria haber cursado 10 o mas semestres, en tal caso debería de haber aprobado todas las asignaturas)
+				if( cin.good() and salon[i].semestres > 0 and salon[i].semestres < 10 ) {
+					semestresValidos = true;																			// El booleano semestresValidos es verdadero, indicando que la entrada es valida y que se continue con la ejecucion del programa
+				} else {																								// SINO
+					clearInputShowErrorMessage("Error...La cantidad de semestres debe estar entre 1 y 9."); 			// Si hay algun error, limpiamos el buffer de entrada y mostramos el respectivo mensaje al usuario
 				}
-			} while(!semestresValidos);																				// Si la entrada no es valida, continuamos pidiendo la cantidad de profesores
-			//** Edad del profesor(a) **//
+			} while(!semestresValidos);																					// Si la entrada no es valida, continuamos pidiendo la cantidad de semestres
+			//** Semestres cursados **//
 
+			//** Promedio del estudiante **//
+			promValido = false;																							// En cada lectura inicializamos la validez del promedio en FALSO
+			do {
+				system("cls");																							// Limpiamos la pantalla para mejor visualizacion
+				cout << "[" << i+1 << "] Ingrese el promedio para " << salon[i].nombre << ": ";							// Mostramos el mensaje al usuario pidiendo el promedio del estudiante
+				cin >> salon[i].promedio;																				// Guardamos el valor ingresado en la posicion correspondiente del vector,
+																														// y asignamos al miembro correspondiente de la estructura
+				//Si todo va bien en la entrada de datos y el dato es un entero entre 0 y 20 (promedio en base bigesimal)
+				if( cin.good() and salon[i].promedio >= 0 and salon[i].promedio <= 20 ) {
+					promValido = true;																					// El booleano semestresValidos es verdadero, indicando que la entrada es valida y que se continue con la ejecucion del programa
+				} else {																								// SINO
+					clearInputShowErrorMessage("Error...El promedio debe estar entre 0 y 20."); 						// Si hay algun error, limpiamos el buffer de entrada y mostramos el respectivo mensaje al usuario
+				}
+			} while(!promValido);																						// Si la entrada no es valida, continuamos pidiendo el promedio del estudiante
+			//** Promedio del estudiante **//
 		}
 		/** Recorremos el arreglo para solicitar los datos al usuario **/
 	///*************** INGRESO DE DATOS ***************///
@@ -104,48 +123,58 @@ int main() {
 
 
 	///*************** PROCESAMIENTO DE DATOS ***************///
-		/** Calculo del promedio de edades **/
-		for(int i=0; i<cantEst; i++) {																				// Recorremos el arreglo
-			edadPromedio += salon[i].edad;																	// para obtener la suma de todas las edades
-		}
-		edadPromedio /= cantEst;																					// Dividimos la suma de todas las edades entre la cantidad de profesores
-		/** Calculo del promedio de edades **/
-
-		/** Calculo de profesor mas joven **/
-		for(int i=0; i<cantEst; i++) {																				// Recorremos el arreglo
-			if(salon[i].edad < edadMinima) {																// Si la edad del docente es menor a la edad minmima (valor inicial es 999)
-				edadMinima = salon[i].edad;																	// ahora la edad minima es la edad del docente
-				memcpy(nombreProfJoven, salon[i].nombre, sizeof(nombreProfJoven));							// Y tambien guardamos el nombre del docente con la funcion memcpy copiamos el arreglo de chars, 
-																													// que es el nombre, ya que no se puede hacer una asignacion directa con el operador "="
+		/** Calculo de estudiante con mejor promedio **/
+		for(int i=0; i<cantEst; i++) {																		// Recorremos el arreglo
+			if(salon[i].promedio > mejorProm) {																// Si el promedio del estudiante es mayor al mejor promedio (valor inicial es -1)
+				mejorProm = salon[i].promedio;																// ahora el mejor promedio es el promedio del estudiante
+				memcpy(estMejorPromNombre, salon[i].nombre, sizeof(estMejorPromNombre));					// Y tambien guardamos el nombre del estudiante con la funcion memcpy copiamos el arreglo de chars, 
+																											// que es el nombre, ya que no se puede hacer una asignacion directa con el operador "="
+				estMejorPromDNI = salon[i].dni;																// Tambien guardamos el DNI del estudiante 																											
 			}
 		}
-		/** Calculo de profesor mas joven **/
+		/** Calculo de estudiante con mejor promedio **/
 
-		/** Calculo de profesor de mas edad **/
-		for(int i=0; i<cantEst; i++) {																				// Recorremos el arreglo
-			if(salon[i].edad > edadMaxima) {																// Si la edad del docente es mayor a la edad maxima (valor inicial es -1)
-				edadMaxima = salon[i].edad;																	// ahora la edad maxima es la edad del docente
-				memcpy(nombreProfMayor, salon[i].nombre, sizeof(nombreProfMayor));							// Y tambien guardamos el nombre del docente con la funcion memcpy copiamos el arreglo de chars, 
-																													// que es el nombre, ya que no se puede hacer una asignacion directa con el operador "="
+		/** Calculo de estudiantes con promedio mayores o iguales a 10.5 **/
+		// Primero debemos saber la cantidad de estudiantes que cumplen esta condicion:
+		for(int i=0; i<cantEst; i++) {																		// Recorremos el arreglo
+			if(salon[i].promedio >= 10.5) {																	// Si el promedio del estudiante es mayor o igual a 10.5
+				cantEstAprobados++;																			// Incrementamos el contador correspondiente
 			}
 		}
-		/** Calculo de profesor de mas edad **/
 
-		/** Calculo de profesoras mujeres con edad mayor al promedio **/
-		for(int i=0; i<cantEst; i++) {																						// Recorremos el arreglo
-			if( (salon[i].sexo == 'f' or salon[i].sexo == 'F') and salon[i].edad > edadPromedio) {	// Si es profesora mujer y con edad mayor al promedio
-				cantProfesorasMayorProm++;																					// Incrementamos el contador correspondiente
+		// Declaramos un vector de punteros de tipo 'estudiante2' de longitud definida por la cantidad de estudiantes con promedio mayor o igual a 10.5, calculada anteriormente
+		estudiante2 *estAprobados = new estudiante2[cantEstAprobados];
+
+		pos = 0;																							// Inicializamos pos en 0, para recorrer el vector que almacena los datos de estudiantes aprobados
+		for(int i=0; i<cantEst; i++) {																		// Recorremos el arreglo
+			if(salon[i].promedio >= 10.5) {																	// Si el promedio del estudiante es mayor o igual a 10.5
+				memcpy(estAprobados[pos].nombre, salon[i].nombre, sizeof(estAprobados[pos].nombre));		// Copiamos el nombre del estudiante al arreglo correspondiente y al miembro correspondiente de la estructura
+				estAprobados[pos].dni = salon[i].dni;														// Copiamos el DNI del estudiante al arreglo correspondiente y al miembro correspondiente de la estructura
+				pos++;																						// Incrementamos pos, para seguir almacenando datos en la siguiente posicion
 			}
 		}
-		/** Calculo de profesoras mujeres con edad mayor al promedio **/
+		/** Calculo de estudiantes con promedio mayores o iguales a 10.5 **/
 
-		/** Calculo de profesores hombres con edad menor al promedio **/
-		for(int i=0; i<cantEst; i++) {																						// Recorremos el arreglo
-			if( (salon[i].sexo == 'm' or salon[i].sexo == 'M') and salon[i].edad < edadPromedio) {	// Si es profesor hombre y con edad menor al promedio
-				cantProfesoresMenorProm++;																					// Incrementamos el contador correspondiente
+		/** Calculo de estudiantes con menos de 5 semstres cursados **/
+		// Primero debemos saber la cantidad de estudiantes que cumplen esta condicion:
+		for(int i=0; i<cantEst; i++) {																		// Recorremos el arreglo
+			if(salon[i].semestres < 5) {																	// Si la cantidad de semestres cursados es menor a 5
+				cantEstMenosMediaCarrera++;																	// Incrementamos el contador correspondiente
 			}
 		}
-		/** Calculo de profesoras mujeres con edad mayor al promedio **/
+
+		// Declaramos un vector de punteros de tipo 'estudiante2' de longitud definida por la cantidad de estudiantes con menos de 5 semstres cursados, calculada anteriormente
+		estudiante2 *estMenosMediaCarrera = new estudiante2[cantEstMenosMediaCarrera];
+
+		pos = 0;																											// Inicializamos pos en 0, para recorrer el vector que almacena los datos de estudiantes con menos de 5 semstres cursados
+		for(int i=0; i<cantEst; i++) {																							// Recorremos el arreglo
+			if(salon[i].semestres < 5) {																					// Si la cantidad de semestres cursados es menor a 5
+				memcpy(estMenosMediaCarrera[pos].nombre, salon[i].nombre, sizeof(estMenosMediaCarrera[pos].nombre));		// Copiamos el nombre del estudiante al arreglo correspondiente y al miembro correspondiente de la estructura
+				estMenosMediaCarrera[pos].dni = salon[i].dni;																// Copiamos el DNI del estudiante al arreglo correspondiente y al miembro correspondiente de la estructura
+				pos++;																										// Incrementamos pos, para seguir almacenando datos en la siguiente posicion
+			}
+		}
+		/** Calculo de estudiantes con menos de 5 semstres cursados **/
 	///*************** PROCESAMIENTO DE DATOS ***************///
 
 
@@ -156,13 +185,14 @@ int main() {
 		for(int i=0;i<80;i++) cout<<"=";								// Mostramos
 		cout << "\n\t\t\t\tDATOS INGRESADOS\n";							// un encabezado
 		for(int i=0;i<80;i++) cout<<"=";								// para la seccion de datos
-		cout <<"\nN - NOMBRE - SEXO - EDAD\n";							// ingresados
+		cout <<"\nN - NOMBRE - DNI - SEMESTRES - PROMEDIO\n";			// ingresados
 
 		/** Datos Ingresados **/
-		for(int i=0; i<cantEst; i++) {																				// Recorremos el arreglo
-			cout << i+1 << " - " << salon[i].nombre << " - ";												// Para cada elemento del arreglo mostramos cada uno de los miembros de la estructura
-			(salon[i].sexo=='f' or salon[i].sexo=='F')? cout << "Femenino" : cout<<"Masculino";		// para el sexo, mostramos la palabra completo dependiendo del caracter almacenado
-			cout << " - " << salon[i].edad << "\n";
+		for(int i=0; i<cantEst; i++) {									// Recorremos el arreglo
+			cout << i+1 << " - " << salon[i].nombre << " - ";			// Mostramos el nombre
+			cout << salon[i].dni << " - ";								// Mostramos el DNI
+			cout << salon[i].semestres << " - ";						// Mostramos los semestres
+			cout << salon[i].promedio << "\n";							// Mostramos el promedio
 		}
 		for(int i=0;i<80;i++) cout<<"=";
 		/** Datos Ingresados **/
@@ -172,11 +202,16 @@ int main() {
 		for(int i=0;i<80;i++) cout<<"=";																				// Mostramos							
 		cout << "\n\t\t\t\tRESULTADOS\n";																				// un encabezado
 		for(int i=0;i<80;i++) cout<<"=";																				// para la seccion de resultados
-		cout << "\na) Edad Promedio del grupo de profesores:\t\t\t" << edadPromedio;									// Mostramos la edad promedio de todos los profesores
-		cout << "\nb) El profesor mas joven del grupo es:\t\t\t\t"<< nombreProfJoven;									// Mostramos el nombre del profesor mas joven
-		cout << "\nc) El profesor de mas edad del grupo es:\t\t\t"<< nombreProfMayor;									// Mostramos el nombre del profesor de mas edad
-		cout << "\nd) El numero de profesoras con edad mayor al promedio es:\t"<< cantProfesorasMayorProm;				// Mostramos la cantidad de profesoras con edad mayor al promedio
-		cout << "\ne) El numero de profesores con edad menor al promedio es:\t"<< cantProfesoresMenorProm << endl;		// Mostramos la cantidad de profesores con edad menor al promedio
+		cout << "\na) nombre y DNI del estudiante que tiene mejor promedio:\t\t\t" << estMejorPromNombre << " - " << estMejorPromDNI;			// Mostramos el nombre y DNI de los estudiantes con mejor promedio
+		cout << "\nb) Listado de nombre y DNI de estudiantes con promedios mayores o iguales a 10.5\n";											// Mostramos el listado de nombre y DNI de estudiantes con promedios mayores o iguales a 10.5
+		for (int i = 0; i < cantEstAprobados; i++) {																							// Recorremos el arreglo que almacena la data de estudiantes aprobados
+			cout << estAprobados[i].nombre << " - " << estAprobados[i].dni << "\n";																// Mostramos el miembro correspondiente de la estructura
+		}
+		
+		cout << "\nc) Listado de nombre y DNI de estudiantes que han cursado menos de 5 semestres hasta el momento:\n";							// Mostramos el nombre y DNI de los estudiantes con menos de 5 semestres cursados
+		for (int i = 0; i < cantEstMenosMediaCarrera; i++) {																					// Recorremos el arreglo que almacena la data de estudiantes con menos de 5 semestres cursados
+			cout << estMenosMediaCarrera[i].nombre << " - " << estMenosMediaCarrera[i].dni << "\n";												// Mostramos el miembro correspondiente de la estructura
+		}
 		for(int i=0;i<80;i++) cout<<"=";
 		/** Resultados **/
 	///*************** VISUALIZACION DE DATOS ***************///

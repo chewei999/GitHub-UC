18061e0ee5025ab9e5d245b7daef4f5e0d8af0d9
chewei999 <chewei999@gmail.com>
2022-10-21 19:06:22 -0500

    Apache

---
+++ POO/ExamenFinal/POO_EvalFinal_JCHM/src/poo_evalfinal_jchm/cConnection.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo_evalfinal_jchm;
+
+import java.sql.*;
+
+/**
+ *
+ * @author JOSE
+ */
+public class cConnection {
+    private String url = "jdbc:ucanaccess://E:\\UC\\GitHub-UC\\POO\\ExamenFinal\\POO_EvalFinal_JCHM\\BookStoreSAC_new.accdb";
+    private String usr = "";
+    private String pswd = "";
+    private Connection con = null;
+    
+    public cConnection() {
+        loadDriver();
+    }
+    
+    private void loadDriver() {
+        try {
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+        }
+        catch(ClassNotFoundException e) {
+            System.out.println("Error al crear el puente JDBC-Access");
+        }
+    }
+    
+    public Connection ObtenerConexion() {
+        System.out.println("Estableciendo conexion con " + url);
+        try {
+            con = DriverManager.getConnection(url, usr, pswd);
+        }
+        catch(SQLException sqle) {
+            System.out.println("No se pudo establecer la conexion");
+            return null;
+        }
+        
+        System.out.println("Conexion establecida con: " + url);
+        return con;
+    }
+    
+    public boolean closeConnection() {
+        try {
+            con.close();
+        }
+        catch(SQLException sqle) {
+            System.out.println("No se cerró la conexion");
+            return false;
+        }
+        System.out.println("Conexion cerrada con exito");
+        return true;
+    }
+}
